3770a564aad1688598a408f4da0f6b832e991758
dylan <kws60000@naver.com>
2020-08-10 20:16:39 +0900

    프로젝트 프로필 이미지를 변경하는 액션을 따로 파생시키고, 사용자 정보 및 프로필, 배경 사진에 대한 초깃값을 설정합니다.

--- src/modules/addWho.js
+++ src/modules/addWho.js
@@ -1,41 +1,51 @@
+import profileImg from "../assets/profile.png";
+import backgroundImg from "../assets/background.jpg";
+
 export const ADDNAME = "addName/ADDWHO";
+export const ADDPROFILE = "addProfile/ADDPROFILE";
 
 let whoId = 1;
-export const addName = ({
-  name,
-  home,
-  hometown,
-  job,
-  school,
-  profile,
-  background,
-}) => ({
+export const addName = ({ name, home, hometown, job, school }) => ({
   type: ADDNAME,
-  whoId: whoId++,
   name,
   home,
   hometown,
   job,
   school,
-  profile,
-  background,
 });
-// ADDNAME 액션은 id, 이름, 거주지, 직업, 출신지, 학교, 프로필 사진, 배경사진값을 변수로 사용한다.
 
-export default function addWho(state = [], action) {
+// 프로필 사진을 변경하는 액션 생성 함수
+export const addProfile = (profile) => ({
+  profile,
+});
+
+// state의 초깃값 설정
+const initialState = [
+  {
+    whoId,
+    name: "김우석",
+    home: "제주도",
+    hometown: "부산",
+    job: "교육생",
+    school: "제주대",
+    profile: profileImg,
+    background: backgroundImg,
+  },
+];
+
+// ADDNAME 액션은 id, 이름, 거주지, 직업, 출신지, 학교를 변수로 사용한다.
+export default function addWho(state = initialState, action) {
   switch (action.type) {
     case ADDNAME:
       const result = [
         ...state,
         {
-          whoID: action.whoID,
+          whoId: whoId++,
           name: action.name,
           home: action.home,
           hometown: action.hometown,
           job: action.job,
           school: action.school,
-          profile: action.profile,
-          background: action.background,
         },
       ];
       // 1. 반환하고자 하는 데이터를 변수에 저장
@@ -47,6 +57,15 @@ export default function addWho(state = [], action) {
 
       return result;
     // 3. 갱신된 데이터를 반환
+
+    case ADDPROFILE:
+      const resultProfile = {
+        profile: action.profile,
+      };
+      if (resultProfile.length > 1) {
+        resultProfile.shift();
+      }
+      return resultProfile;
 
     default:
       return state;
